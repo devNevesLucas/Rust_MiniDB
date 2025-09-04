e5817d82c65489787b223c3c7383e0dc02793bab
Lucas Neves <lzmn70@gmail.com>
2025-09-04 10:34:24 -0300

    Desenvolvendo base do projeto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{self};
+
 fn main() {
     println!("Hello, world!");
+
+    let mut input_teclado = String::new();
+
+    loop {
+        println!("Digite o comando:");
+
+        input_teclado.clear();
+
+        io::stdin().read_line(&mut input_teclado)
+            .expect("Erro lendo entrada");
+
+        let input_tratado = input_teclado.trim();
+
+        match input_tratado {
+            "SAIR" => {
+                println!("Saindo...");
+                break;
+            }
+            _ => {
+                println!("Digite um comando válido");
+            }
+        }
+    }
 }
